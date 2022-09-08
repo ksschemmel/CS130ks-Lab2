d93b9fa5eedb33848bcf8fe2f3736a34f3187b72
ksschemmel <112411813+ksschemmel@users.noreply.github.com>
2022-09-08 12:11:35 -0400

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,8 @@
+public class Address
+{
+public static void main(String[]args)
+{
+System.out.println("Bellarmine University\n 2001 Newburg Road\n Louisville,Kentucky 40205");
+}
+
+}
---
+++ MyMondaySchedule.java
@@ -0,0 +1,19 @@
+public class MyMondaySchedule
+{
+public static void main(String[]args)
+{
+System.out.println("On Monday I wake up at "+8+" AM");
+System.out.println("I take a shower at "+"8:30"+" AM");
+System.out.println("I go to Math Class at "+10+" AM");
+System.out.println("I go to Computer Science at "+11+" AM");
+System.out.println("I go to French Class at "+12+" PM");
+System.out.println("I go to lunch at "+1+" PM");
+System.out.println("I go to Centro at "+"1:45"+" PM");
+System.out.println("I go to Philosophy at "+3+" PM");
+System.out.println("I go to History at "+"4:30"+" PM");
+System.out.println("I eat dinner at "+6+" PM");
+System.out.println("I do homework until "+11+" PM");
+System.out.println("I go to sleep at "+"12:30"+" PM");
+}
+
+}
